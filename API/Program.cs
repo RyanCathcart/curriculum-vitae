d107a092c913a51362a2d13b55207d4fb8043b18
Ryan Cathcart <ryanwcathcart@gmail.com>
2022-02-05 21:56:13 -0500

    Created a TypeScript create-react-app and installed Material UI

--- API/Program.cs
+++ API/Program.cs
@@ -19,7 +19,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
     app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv6 v1"));
 }
 
 app.UseHttpsRedirection();
