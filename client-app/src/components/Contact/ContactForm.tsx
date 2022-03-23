59336af1d65e4345c50c19069ff1634f6196f8ce
Ryan Cathcart <ryanwcathcart@gmail.com>
2022-03-23 04:19:39 -0400

    Form validation introduced to the ContactForm with the use of the useForm custom react hook.

--- client-app/src/components/Contact/ContactForm.tsx
+++ client-app/src/components/Contact/ContactForm.tsx
@@ -1,19 +1,50 @@
 import { Box, Button, Container, TextField } from "@mui/material";
 import emailjs from "@emailjs/browser";
+import useForm, { Values } from "../../hooks/useForm";
 
 export default function ContactForm() {
   const serviceID = "service_yqo5rw5";
   const templateID = "template_loaeo0l";
   const userID = "dFpCPYdaMXPW8Jcu7";
 
+  
+
   const sendEmail = (e: any) => {
-    e.preventDefault();
-    emailjs.sendForm(serviceID, templateID, e.target, userID)
-      .then((result) => {
+    emailjs.sendForm(serviceID, templateID, e.target, userID).then(
+      (result) => {
         console.log(result.text);
-      }, (error) => {
+      },
+      (error) => {
         console.log(error.text);
-      });
+      }
+    );
+  };
+
+  const validate = (fieldValues: Values) => {
+    let temp: Values = {...errors};
+    if ("from_name" in fieldValues) {
+      temp.from_name = fieldValues.from_name ? "" : "This field is required";
+    }
+    if ("sender_email" in fieldValues) {
+      temp.sender_email = (/.+@.+..+/).test(fieldValues.sender_email!) ? "" : "Email is not valid";
+    }
+    if ("message" in fieldValues) {
+      temp.message = fieldValues.message ? "" : "This field is required";
+    }
+    setErrors({ ...temp });
+
+    if (fieldValues === values) {
+      return Object.values(temp).every((x) => x === "");
+    }
+  };
+
+  const { values, errors, setErrors, handleInputChange } = useForm(true, validate);
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    if (validate(values)) {
+      sendEmail(e);
+    }
     e.target.reset();
   };
 
@@ -21,17 +52,43 @@ export default function ContactForm() {
     <Container maxWidth="sm" sx={{ display: "flex", flexDirection: "column", alignContent: "center" }}>
       <Box
         component="form"
-        onSubmit={sendEmail}
-        sx={{ 
-          display: "flex", 
-          flexDirection: "column", 
-          alignContent: "center" 
+        onSubmit={handleSubmit}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignContent: "center",
         }}
       >
-        <TextField label="Name" margin="normal" name="from_name" />
-        <TextField label="Email" margin="normal" name="sender_email" />
-        <TextField label="Message" margin="normal" name="message" multiline={true} minRows={4} />
-        <Button type="submit" variant="contained" color="secondary">
+        <TextField
+          label="Name"
+          value={values.from_name}
+          name="from_name"
+          margin="normal"
+          required
+          onChange={handleInputChange}
+          {...(errors.from_name && { error: true, helperText: errors.from_name })}
+        />
+        <TextField
+          label="Email"
+          value={values.sender_email}
+          name="sender_email"
+          margin="normal"
+          required
+          onChange={handleInputChange}
+          {...(errors.sender_email && { error: true, helperText: errors.sender_email })}
+        />
+        <TextField
+          label="Message"
+          value={values.message}
+          name="message"
+          margin="normal"
+          required
+          onChange={handleInputChange}
+          {...(errors.message && { error: true, helperText: errors.message })}
+          multiline={true}
+          minRows={4}
+        />
+        <Button type="submit" variant="contained" color="secondary" sx={{ my: 1 }}>
           Submit
         </Button>
       </Box>
---
+++ client-app/src/hooks/useForm.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+export interface Values {
+  from_name?: any;
+  sender_email?: any;
+  message?: any;
+}
+
+export default function useForm(validateOnChange = false, validate: any) {
+  const [values, setValues] = useState<Values>({
+    from_name: "",
+    sender_email: "",
+    message: ""
+  });
+
+  const [errors, setErrors] = useState<Values>({});
+
+  const handleInputChange = (e: any) => {
+    const {name, value} = e.target;
+    setValues({
+      ...values,
+      [name]: value
+    });
+    if (validateOnChange) {
+      validate({[name]: value})
+    }
+  };
+  
+  return {
+    values,
+    setValues,
+    errors,
+    setErrors,
+    handleInputChange
+  };
+}
