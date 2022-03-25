d6b0ebdc2c2260eda91a5601a4ee46a9c11414c6
Ryan Cathcart <ryanwcathcart@gmail.com>
2022-03-24 21:01:12 -0400

    Added .gif for README.mf and updated README

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
-# Developer Portfolio
+# Ryan Cathcart - Developer Portfolio
+View website: [https://www.ryancathcart.dev/](https://www.ryancathcart.dev/)
 
-This is a simple React application that shows my skillsets as a software developer.
+![Alt text](/dev-demo.gif)
+
+#### About this project:
+This is a simple website made to display my skillsets as a software developer.
+
+
+##### Made using:
+- HTML
+- CSS
+- TypeScript
+- React
+- Material UI
+- EmailJS
+- Mailgun
+- Azure Static Web Apps
--- client-app/src/components/Contact/Contact.tsx
+++ client-app/src/components/Contact/Contact.tsx
@@ -5,8 +5,11 @@ export default function Contact() {
 
   return (
     <Container className="Contact" maxWidth="lg" sx={{ paddingTop: 8, paddingBottom: 6 }}>
-      <Typography variant="h2" color="primary" align="center" sx={{ paddingBottom: 8, fontWeight: "bold" }}>
+      <Typography variant="h2" color="primary" align="center" sx={{ paddingBottom: 1, fontWeight: "bold" }}>
         CONTACT
+      </Typography>
+      <Typography variant="subtitle1" color="primary" align="center" sx={{ paddingBottom: 6 }}>
+        Feel free to contact me by any of my social medias, or by email below:
       </Typography>
       <ContactForm  />
     </Container>
